f0c3246a869a13e43cf19722e512d7b6cbfd41c4
K3vX21 <k3vn.xu@gmail.com>
2021-04-04 13:12:56 -0500

    consolidate scraping and writing

--- read_write_grades.py
+++ read_write_grades.py
@@ -7,46 +7,36 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from secret import IC_ID, IC_PW, SHEET_NAME
-from schemas import Student, Course, Grade
-from create_profile import login
-from datetime import date
+from datetime import datetime
+from login import login 
 import gspread
-import schemas
-import pandas as pd
 
-driver = webdriver.Chrome()
+def add_grades(): # get list of course grades
+    driver = webdriver.Chrome()
+    login(driver)
 
-login(driver)
+    driver.get("https://infinitecampus.naperville203.org/campus/nav-wrapper/student/portal/student/grades")
+    time.sleep(1)
+    driver.switch_to.frame(0)
+    time.sleep(1)
+    grade_finder = driver.find_elements(By.TAG_NAME, "div")
 
-driver.get("https://infinitecampus.naperville203.org/campus/nav-wrapper/student/portal/student/grades")
-time.sleep(1)
-driver.switch_to.frame(0)
-time.sleep(1)
-grade_finder = driver.find_elements(By.TAG_NAME, "div")
-print("Cards:")
-print()
+    daily_grades = []
+    date_time = datetime.now().strftime("%m/%d/%Y")    
+    daily_grades.append(date_time)
+    
+    for element in grade_finder:
+        if element.get_attribute('class') == 'grades__flex-row__item grades__flex-row__item--left':
+            #  print(element.text)
+            if element.text == "Semester Grade":
+                row_info = element.find_element_by_xpath('..')
+                # print("ya")
+                semester_grade = row_info.find_element_by_class_name("grading-score__row-spacing")
+                grade_as_num = semester_grade.text.strip("(%)")
+                daily_grades.append(float(grade_as_num))
 
-counter = 0
-for element in grade_finder:
-    if element.get_attribute('class') == 'grades__flex-row__item grades__flex-row__item--left':
-        #  print(element.text)
-        if element.text == "Semester Grade":
-            row_info = element.find_element_by_xpath('..')
-            # print("ya")
-            semester_grade = row_info.find_element_by_class_name("grading-score__row-spacing")
-            grade_as_int = semester_grade.text.strip("(%)")
-            print(element.text + ":", grade_as_int)
-            
-    counter += 1
+    gc = gspread.service_account(filename="creds.json")
+    sh = gc.open(SHEET_NAME).sheet1
+    sh.append_row(daily_grades)
 
-
-gc = gspread.service_account(filename="creds.json")
-sh = gc.open(SHEET_NAME).sheet1
-
-"""
-daily_grades = []
-for course in student.courses:
-    daily_grades.append(course.get_grade(-1))
-
-sh.append_row(daily_grades)
-"""
+add_grades()
