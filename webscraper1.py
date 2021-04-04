386db8df9f1979259ec197276479c8f41fc5f0c2
K3vX21 <k3vn.xu@gmail.com>
2021-04-04 00:48:36 -0500

    clean up and swtich into iframe

--- webscraper1.py
+++ webscraper1.py
@@ -23,32 +23,25 @@ password.send_keys(IC_PW)
 driver.find_element_by_xpath("//input[@type='submit']").click()
 
 driver.get("https://infinitecampus.naperville203.org/campus/nav-wrapper/student/portal/student/grades")
+
 time.sleep(2)
-# search_grades_card = driver.find_elements_by_class_name("ellipsis-container")
-search_grades_card = driver.find_elements(By.TAG_NAME, "div") # driver.find_elements_by_tag_name('a')
+
+# size = len(driver.find_elements(By.TAG_NAME, "iframe"))
+
+driver.switch_to.frame(0)
+
+table_rows = driver.find_elements(By.CLASS_NAME, "grades__row clickable router-link-reset ng-star-inserted")
 print("Cards:")
 print()
-#type(search_grades_card[0])
-#print(search_grades_card[1].get_attribute('class'))
-# print(len(search_grades_card))
 
-for element in search_grades_card:
+for element in table_rows:
     print(element.get_attribute('class'))
     print(element.get_attribute('id'))
-    print()
+    for subelement in element.find_elements(By.TAG_NAME,"div"):
+        print("\n" + subelement.get_attribute('class'))
+        print("\n" + subelement.get_attribute('id'))
+    print("=====")
+
 print("No more script")
-time.sleep(300)
 
-"""
-delay = 30
-try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'collapsible-card grades__card')))
-    print("Page is ready!")
-    print(myElem)
-except TimeoutException:
-    print("Loading took too much time!")
-"""
-
-# elements = driver.find_elements_by_class_name("grading-score__row-spacing")
-# print(len(elements))
-# driver.find_element_by_name(name)
+time.sleep(5)
