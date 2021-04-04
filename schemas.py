da7f0aacae1808e901991e187ba935339acb5187
K3vX21 <k3vn.xu@gmail.com>
2021-04-04 01:15:10 -0500

    complete schemas (not tested yet)

---
+++ schemas.py
@@ -0,0 +1,47 @@
+class Student:
+    def __init__(self, first="", last=""):
+        self.first_name = first
+        self.last_name = last
+        self.courses = []
+
+    def add_course(Course course)
+        self.courses.append(courses)
+
+class Course:
+    def __init__(self, name=""):
+        self.course_name = name
+        self.course_grades = []
+    
+    def get_name():
+        return self.course_name
+
+    def set_name(new_name):
+        self.course_name = new_name
+    
+    def get_grade(index):
+        if (index >= len(self.course_grades)):
+            return null
+        return self_grades[index]
+
+    def add_grade(new_grade):
+        self.course_grades.append(new_grade)
+    
+    def remove_recent_grade():
+        self.course_grades.pop(-1)
+
+class Grade:
+    def __init__(self, date, decimal=0.0):
+        self.date = date
+        self.score = decimal
+    
+    def get_date():
+        return self.date
+
+    def set_date(new_date):
+        self.date = new_date
+
+    def get_score():
+        return self.date
+
+    def set_score(new_decimal):
+        self.score = new_decimal
